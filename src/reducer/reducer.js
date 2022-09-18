945a8f0a0bf8eca112d2cf693ef5b164de1db57b
Rob Hicks <robert.hicks@familysearch.org>
2022-09-17 23:09:56 -0600

    clean up reducer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -5,7 +5,7 @@ const copy = (obj) => JSON.parse(JSON.stringify(obj))
 const key = 'budget'
 
 const reducer = (state, action) => {
-  console.log('action', action)
+  // console.log('action', action)
   switch (action.type) {
     case 'LOAD_FROM_STORAGE': {
       return { ...state, ...action.data }
@@ -106,28 +106,6 @@ const reducer = (state, action) => {
       set(key, newState)
       return newState
     }
-
-    case 'changeName':
-      return { ...state, profile: { ...state.profile, name: action.data } }
-    case 'changeOwner':
-      return { ...state, profile: { ...state.profile, owner: action.data } }
-    case 'updateInstructions': {
-      if (action.data.type === 'userInstructions') {
-        return { ...state, userInstructions: { content: action.data.content, update: new Date() } }
-      } else if (action.data.type === 'reviewerInstructions') {
-        return { ...state, reviewerInstructions: { content: action.data.content, update: new Date() } }
-      } else {
-        return { ...state, warningInstructions: { content: action.data.content, update: new Date() } }
-      }
-    }
-    case 'updateField': {
-      const st = copy(state)
-      const fieldset = st.fieldsets.find((fs) => fs.id === action.data.fieldset.id)
-      const field = fieldset.fields.find((f) => f.id === action.data.field.id)
-      field.help = action.data.update
-      return { ...state, ...st }
-    }
-
     default:
       throw new Error()
   }
